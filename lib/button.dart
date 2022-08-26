f4754a461b210ebe3a81ea0c615ebc07a27c2762
valas <larsien85@gmail.com>
2022-08-26 15:41:36 +0900

    Update button.dart

--- lib/button.dart
+++ lib/button.dart
@@ -1,33 +1,28 @@
+import 'dart:developer';
+
 import 'package:flame/components.dart';
-import 'package:flame/events.dart';
 import 'package:flame/experimental.dart';
-import 'package:flame/flame.dart';
-import 'package:flutter_game_test/dino_player.dart';
 import 'package:flutter_game_test/game_state.dart';
-import 'package:flutter_game_test/option.dart';
 
 import 'constsnts.dart';
 
 class Button extends SpriteComponent with HasGameRef, TapCallbacks {
   // late Sprite _floorSprite;
   Button() : super();
-  final double xSize = 2400;
-  final double ySize = 24;
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    // final image = await Flame.images.load(Constants.finn);
     // sprite =
     // Sprite(image, srcPosition: Vector2(2, 104), srcSize: Vector2(2400, 24));
     // Sprite(image,
     //     srcPosition: Vector2(2, 104), srcSize: Vector2(xSize, ySize));
-    sprite = await gameRef.loadSprite(Constants.finn);
+    sprite = await gameRef.loadSprite(Constants.buttonImage);
     // position =
     // anchor = Anchor.center;
     position = Vector2(0, gameRef.size.y / 2);
-    size = Vector2(gameRef.size.x, gameRef.size.y / 2);
+    size = Vector2(gameRef.size.x, gameRef.size.y / 4);
     // position = Vector2(0, gameRef.size.y - sprite!.srcSize.y);
-    print("Button onLoad : $position, ${gameRef.size}");
+    log("Button onLoad : $position, ${gameRef.size}");
   }
 
   @override
@@ -41,15 +36,19 @@ class Button extends SpriteComponent with HasGameRef, TapCallbacks {
         break;
       case GameState.gameover:
         break;
+      case GameState.fever:
+        break;
+      case GameState.invulnerable:
+        break;
     }
   }
 
   @override
-  void onGameResize(Vector2 gameSize) {
-    super.onGameResize(gameSize);
+  void onGameResize(Vector2 size) {
+    super.onGameResize(size);
     // We don't need to set the position in the constructor, we can set it directly here since it will
     // be called once before the first time it is rendered.
     // position = gameSize / 2;
-    print("gameSize : $gameSize");
+    log("gameSize : $size");
   }
 }
