a01681df6acd5bcb2db03e29bd3a064c63e55a05
valas <larsien85@gmail.com>
2022-08-26 15:42:22 +0900

    Update constsnts.dart

--- lib/constsnts.dart
+++ lib/constsnts.dart
@@ -1,5 +1,5 @@
 class Constants {
   static const String spriteAsset = 'dino-sprite.png';
-  static const String finn = 'finn.png';
-  // static const String finn = 'assets/images/finn.png';
+  static const String buttonImage = 'button.png';
+  static const String overlayName = "PauseMenu";
 }
